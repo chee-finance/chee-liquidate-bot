fb1f071e07c07528b199eb981bea4de945cc0954
0xChee <dev@chee.finance>
2022-01-26 23:45:21 +0800

    feat: change naming

--- index.js
+++ index.js
@@ -5,64 +5,64 @@ const getAccount = require('./getAccount').getAccount
 const web3 = new Web3('https://alfajores-forno.celo-testnet.org')
 const kit = ContractKit.newKitFromWeb3(web3)
 
-const VTokenAbi = require('./abi/VToken.json')
-const Bep20Abi = require('./abi/Bep20.json')
+const CTokenAbi = require('./abi/CToken.json')
+const CBep20Abi = require('./abi/CBep20.json')
 
 const args = process.argv.slice(2);
 // const borrower = '0x31bE3B3F802886Fac0a1E0cf0984B7e6734f9e1D';
-// const vToken = '0xD927502Da7A7759A132469212c7fa69d2198e344';
+// const cToken = '0xD927502Da7A7759A132469212c7fa69d2198e344';
 // const underlying = '0xD98B00822A950F09F5C4De6B081A67764519a286';
 // const collateral = '0xa86b824475C6C697E9d4A819E111dd3236b089C3';
 const defaultAmount = web3.utils.toWei('50000000000000');
 // const repayAmount = web3.utils.toWei('0.01');
 
-const [borrower, vToken, underlying, collateral, repayAmount] = args
+const [borrower, cToken, underlying, collateral, repayAmount] = args
 
-async function initContract(){
-    if (args.length < 5) {
-        throw('missing liquidator parameters!')
-    }
-    let instance = new web3.eth.Contract(
-        VTokenAbi,
-        vToken
-    );
+async function initContract() {
+  if (args.length < 5) {
+    throw ('missing liquidator parameters!')
+  }
+  let instance = new web3.eth.Contract(
+    CTokenAbi,
+    cToken
+  );
 
-    let token = new web3.eth.Contract(
-        Bep20Abi,
-        underlying
-    );
+  let token = new web3.eth.Contract(
+    CBep20Abi,
+    underlying
+  );
 
-    getName(token)
-    liquidateBorrow(instance, token)
+  getName(token)
+  liquidateBorrow(instance, token)
 }
 
-async function getName(instance){
-    let name = await instance.methods.name().call()
-    console.log(name)
+async function getName(instance) {
+  let name = await instance.methods.name().call()
+  console.log(name)
 }
 
-async function liquidateBorrow(instance, token){
-    try {
-        let account = await getAccount()
-        const amount = web3.utils.toWei(repayAmount)
+async function liquidateBorrow(instance, token) {
+  try {
+    let account = await getAccount()
+    const amount = web3.utils.toWei(repayAmount)
 
-        kit.connection.addAccount(account.privateKey)
-    
-        // approve
-        let txObject = await token.methods.approve(vToken, defaultAmount)
-        let tx = await kit.sendTransactionObject(txObject, { from: account.address })
-        receipt = await tx.waitReceipt()
-        console.log('approve finished!')
-        
-        // liquidate
-        txObject = await instance.methods.liquidateBorrow(borrower, amount, collateral)
-        tx = await kit.sendTransactionObject(txObject, { from: account.address })
-        receipt = await tx.waitReceipt()
-        console.log(`liquidate ${borrower} finished!`)
+    kit.connection.addAccount(account.privateKey)
 
-    } catch (e) {
-        console.log(e, 'tx failed')
-    }
+    // approve
+    let txObject = await token.methods.approve(cToken, defaultAmount)
+    let tx = await kit.sendTransactionObject(txObject, { from: account.address })
+    receipt = await tx.waitReceipt()
+    console.log('approve finished!')
+
+    // liquidate
+    txObject = await instance.methods.liquidateBorrow(borrower, amount, collateral)
+    tx = await kit.sendTransactionObject(txObject, { from: account.address })
+    receipt = await tx.waitReceipt()
+    console.log(`liquidation ${borrower} finished!`)
+
+  } catch (e) {
+    console.log(e, 'tx failed')
+  }
 }
 
 initContract().catch(console.log)
