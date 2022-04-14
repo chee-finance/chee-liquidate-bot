c1d12cfe44991214120d542d9ba4223805cad702
ddd <dongshuxiu@blued.com>
2022-04-14 17:10:43 +0800

    feat: init

---
+++ constants.js
@@ -0,0 +1,12 @@
+const SUBGRAPH_URL = {
+  CELO: 'https://api.thegraph.com/subgraphs/name/cheenicey/chee-subgraph-alfajores',
+  BSC: 'https://api.thegraph.com/subgraphs/name/cheenicey/chee-subgraph-bsc0',
+  METER: '',
+  POLYGON: 'https://api.thegraph.com/subgraphs/name/cheenicey/chee-subgraph-mumbai',
+};
+
+
+
+module.exports = {
+  SUBGRAPH_URL
+}
