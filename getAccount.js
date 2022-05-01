ecaccb5bf3a00fd516f884b1f0118ee1e588d5fc
0xChee <dev@chee.finance>
2022-05-01 17:48:03 +0800

    add eslint

--- getAccount.js
+++ getAccount.js
@@ -1,22 +1,22 @@
 const Web3 = require('web3')
 const fs = require('fs')
 const path = require('path')
-var web3 = new Web3()
+const web3 = new Web3()
 
 const filePath = path.join(__dirname, './.secret')
 
-function getAccount() {
+function getAccount () {
   return new Promise(resolve => {
     if (fs.existsSync(filePath)) {
-      fs.readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
+      fs.readFile(filePath, { encoding: 'utf-8' }, (_err, data) => {
         resolve(web3.eth.accounts.privateKeyToAccount(data))
       })
     } else {
-      let randomAccount = web3.eth.accounts.create()
+      const randomAccount = web3.eth.accounts.create()
 
       fs.writeFile(filePath, randomAccount.privateKey, (err) => {
         if (err) {
-          return console.log(err);
+          return console.log(err)
         }
       })
       resolve(randomAccount)
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Web3 = require("web3")
+const Web3 = require('web3')
 const ContractKit = require('@celo/contractkit')
 const getAccount = require('./getAccount').getAccount
 
@@ -8,42 +8,43 @@ const kit = ContractKit.newKitFromWeb3(web3)
 const CTokenAbi = require('./abi/CToken.json')
 const CBep20Abi = require('./abi/CBep20.json')
 
-const args = process.argv.slice(2);
+const args = process.argv.slice(2)
 // const borrower = '0x31bE3B3F802886Fac0a1E0cf0984B7e6734f9e1D';
 // const cToken = '0xD927502Da7A7759A132469212c7fa69d2198e344';
 // const underlying = '0xD98B00822A950F09F5C4De6B081A67764519a286';
 // const collateral = '0xa86b824475C6C697E9d4A819E111dd3236b089C3';
-const defaultAmount = web3.utils.toWei('50000000000000');
+const defaultAmount = web3.utils.toWei('50000000000000')
 // const repayAmount = web3.utils.toWei('0.01');
 
 const [borrower, cToken, underlying, collateral, repayAmount] = args
 
-async function initContract() {
+async function initContract () {
   if (args.length < 5) {
+    // eslint-disable-next-line no-throw-literal
     throw ('missing liquidator parameters!')
   }
-  let instance = new web3.eth.Contract(
+  const instance = new web3.eth.Contract(
     CTokenAbi,
     cToken
-  );
+  )
 
-  let token = new web3.eth.Contract(
+  const token = new web3.eth.Contract(
     CBep20Abi,
     underlying
-  );
+  )
 
   getName(token)
   liquidateBorrow(instance, token)
 }
 
-async function getName(instance) {
-  let name = await instance.methods.name().call()
+async function getName (instance) {
+  const name = await instance.methods.name().call()
   console.log(name)
 }
 
-async function liquidateBorrow(instance, token) {
+async function liquidateBorrow (instance, token) {
   try {
-    let account = await getAccount()
+    const account = await getAccount()
     const amount = web3.utils.toWei(repayAmount)
 
     kit.connection.addAccount(account.privateKey)
@@ -51,15 +52,14 @@ async function liquidateBorrow(instance, token) {
     // approve
     let txObject = await token.methods.approve(cToken, defaultAmount)
     let tx = await kit.sendTransactionObject(txObject, { from: account.address })
-    receipt = await tx.waitReceipt()
+    await tx.waitReceipt()
     console.log('approve finished!')
 
     // liquidate
     txObject = await instance.methods.liquidateBorrow(borrower, amount, collateral)
     tx = await kit.sendTransactionObject(txObject, { from: account.address })
-    receipt = await tx.waitReceipt()
+    await tx.waitReceipt()
     console.log(`liquidation ${borrower} finished!`)
-
   } catch (e) {
     console.log(e, 'tx failed')
   }
