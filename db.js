7208416913d7d1473a6b9b0344e7c2ed6fc98443
ddd <dongshuxiu@blued.com>
2022-08-30 17:44:08 +0800

    feat: add local db connect

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const ssh2mysql = require("ssh2-connect-mysql");
 const fs = require("fs");
+const mysql = require('mysql');
 require('dotenv').config()
-console.log('process.env.DB_SSH_USER',process.env.DB_SSH_USER)
+
 const ssh_conf = {
   host: process.env.DB_SSH_HOST,
   port: 22,
@@ -14,21 +15,56 @@ const db_conf = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 };
-let query = function (sqlVal) {
+// connect remote db
+// let query = function (sqlVal) {
+//   return new Promise((resolve, reject) => {
+//     ssh2mysql.connect(ssh_conf, db_conf).then((sql) => {
+//       sql.query(sqlVal, (err, rows) => {
+//         if (err) {
+//           reject(err);
+//         } else {
+//           resolve(rows);
+//         }
+//         // ssh2mysql.close();
+//       });
+//     });
+//   });
+// };
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  port: 3306,
+  user: 'root', 
+  password: 'cheemysql', 
+  database: 'chee', 
+});
+
+
+function query(sqlVal) {
   return new Promise((resolve, reject) => {
-    ssh2mysql.connect(ssh_conf, db_conf).then((sql) => {
-      sql.query(sqlVal, (err, rows) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(rows);
-        }
-        // ssh2mysql.close();
-      });
+    pool.getConnection(function (err, connection) {
+      if (err) {
+        reject(err);
+        resolve({ status: 500, message: 'connect error' });
+      } else {
+        connection.query(sqlVal, function (err, rows) {
+          if (err) {
+            reject(err);
+            resolve({
+              status: 400,
+              message: 'error',
+            });
+          } else {
+            connection.release();
+            resolve({
+              rows,
+            });
+          }
+        });
+      }
     });
   });
-};
-
+}
 async function getDbData(sql) {
   let data = await query(sql);
   return data;
